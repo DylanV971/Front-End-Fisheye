8d60d50904d8a04434a54e09c25e63d067a5db22
Dylan <ventdylan@gmail.com>
2021-11-19 12:16:06 -0500

    (profile pictures and profile descriptions) added

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,30 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, city, country, tagline, price  } = data;
+    
     const picture = `assets/photographers/${portrait}`;
-
+    
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const link = document.createElement('a');
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const h3 = document.createElement('h3');
+        const paragraph = document.createElement('p');
+        const span = document.createElement('span');
+        link.setAttribute("href", "#");
+        link.setAttribute( "aria-label", "Lien page " + name);
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "photo profil " + name);
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        h3.textContent = city + ", " + country;
+        paragraph.textContent = tagline;
+        span.textContent = price +"€/jour" ;
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(link);
+        article.appendChild(h3);
+        article.appendChild(paragraph);
+        article.appendChild(span);
         return (article);
     }
     return { name, picture, getUserCardDOM }
