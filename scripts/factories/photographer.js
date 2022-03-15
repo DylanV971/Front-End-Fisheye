1d56b496b9c099d121413ebeb73c57be23157788
Dylan <ventdylan@gmail.com>
2022-03-14 21:29:59 -0400

    replacement of single quotes by double quotes. use of template literals

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,21 +4,21 @@ function photographerFactory(data) {
     const picture = `assets/photographers/${portrait}`;
     
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const link = document.createElement('a');
-        const img = document.createElement( 'img' );
-        const h2 = document.createElement( 'h2' );
-        const h3 = document.createElement('h3');
-        const paragraph = document.createElement('p');
-        const span = document.createElement('span');
+        const article = document.createElement("article");
+        const link = document.createElement("a");
+        const img = document.createElement("img");
+        const h2 = document.createElement("h2");
+        const h3 = document.createElement("h3");
+        const paragraph = document.createElement("p");
+        const span = document.createElement("span");
         link.setAttribute("href", "#");
-        link.setAttribute( "aria-label", "Lien page " + name);
+        link.setAttribute( "aria-label", `Lien page ${name}`);
         img.setAttribute("src", picture);
-        img.setAttribute("alt", "photo profil " + name);
+        img.setAttribute("alt", `Photo profil  ${name}`);
         h2.textContent = name;
-        h3.textContent = city + ", " + country;
+        h3.textContent = `${city} , ${country}`;
         paragraph.textContent = tagline;
-        span.textContent = price +"€/jour" ;
+        span.textContent = `${price}€/jour` ;
         link.appendChild(img);
         link.appendChild(h2);
         article.appendChild(link);
